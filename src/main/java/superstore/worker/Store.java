27334ade06224f7fb9cb209c9822866d9615bc6a
Colin Alworth <colin@redcurrent.com>
2017-03-23 10:02:36 -0500

    Super minimal compiled JS that can use CQ at all

--- src/main/java/superstore/worker/Store.java
+++ src/main/java/superstore/worker/Store.java
@@ -1,17 +1,66 @@
 package superstore.worker;
 
+import com.google.common.collect.Lists;
+import com.google.gwt.core.client.Callback;
 import com.google.gwt.core.client.EntryPoint;
+import com.googlecode.cqengine.ConcurrentIndexedCollection;
+import com.googlecode.cqengine.IndexedCollection;
+import com.googlecode.cqengine.attribute.Attribute;
+import com.googlecode.cqengine.attribute.SimpleAttribute;
+import com.googlecode.cqengine.attribute.SimpleNullableMapAttribute;
+import com.googlecode.cqengine.index.hash.HashIndex;
+import com.googlecode.cqengine.index.navigable.NavigableIndex;
+import com.googlecode.cqengine.index.unique.UniqueIndex;
+import com.googlecode.cqengine.persistence.onheap.OnHeapPersistence;
+import com.googlecode.cqengine.query.Query;
+import com.googlecode.cqengine.query.QueryFactory;
+import com.googlecode.cqengine.query.option.QueryOptions;
+import com.googlecode.cqengine.resultset.ResultSet;
 
 import java.io.Serializable;
-import java.util.Arrays;
+import java.util.*;
 import java.util.function.BinaryOperator;
 import java.util.function.UnaryOperator;
 
 /**
- * Created by colin on 3/21/17.
+ * Experiments with indexing data in the browser.
  */
 public class Store implements EntryPoint {
-    public void onModuleLoad() {
+    private final IndexedCollection<Map<String, ?>> collection;
+
+    public Store() {
+        collection = new ConcurrentIndexedCollection<>();
+        collection.addIndex(UniqueIndex.onAttribute(makeAttribute("id", String.class)));
+        collection.addIndex(NavigableIndex.onAttribute(makeAttribute("score", Double.class)));
+        collection.addIndex(HashIndex.onAttribute(makeAttribute("street", String.class)));
+    }
+
+    private static <T> Attribute<Map<String, ?>, T> makeAttribute(String key, Class<T> type) {
+        //noinspection unchecked
+        return (Attribute) new SimpleNullableMapAttribute<>(key, type);
+    }
+
+
+
+    public void addItem(Map<String, ?> item) {
+        //noinspection unchecked
+        collection.add(QueryFactory.mapEntity(item));
+    }
+
+    public void query(Query<Map<String, ?>> query, Callback<ArrayList<Map<String, ?>>, Throwable> callback) {
+        try {
+            //TODO consider changing API to stream results instead
+            callback.onSuccess(Lists.newArrayList(collection.retrieve(query)));
+        } catch (Throwable t) {
+            callback.onFailure(t);
+        }
 
     }
+
+    @Override
+    public void onModuleLoad() {
+        addItem(new HashMap<>());
+    }
+
+
 }
